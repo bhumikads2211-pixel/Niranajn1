962a030848fbf775dbf270fdaebc2bc9585476a4
bhumika <bhumikads2211@gmail.com>
2025-09-11 21:20:37 +0530

    Lets see

--- Fort/urls.py
+++ Fort/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from.views import Login 
 urlpatterns =[
     path('login',Login),
+    path('', test_view),
 ]
--- Fort/views.py
+++ Fort/views.py
@@ -4,3 +4,5 @@ from django.template import loader
 def Login(request):
     template = loader.get_template('login.html')
     return HttpResponse(template.render())
+def test_view(request):
+    return HttpResponse("Working")
